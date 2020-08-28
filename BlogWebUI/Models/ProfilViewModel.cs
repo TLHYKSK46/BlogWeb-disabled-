9632042c8389e4be986e80a282484f610f0227e7
Talha YÜKSEK <yuksek_talha@hotmail.com>
2020-08-28 17:47:59 +0300

    profil

---
+++ BlogWebUI/Models/ProfilViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BlogEntities.BlogDb;
+using System.Collections.Generic;
+
+namespace BlogWebUI.Models
+{
+    public class ProfilViewModel
+    {
+        public List<Kullanici> SuperAdmin { get; internal set; }
+    }
+}
---
+++ BlogWebUI/ViewComponents/ProfilKartViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using BlogBusiness.Abstract;
+using BlogDataAccess.Abstract;
+using BlogWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebUI.ViewComponents
+{
+    public class ProfilKartViewComponent:ViewComponent
+    {
+        IKullaniciServis _kullaniciServis;
+
+        public ProfilKartViewComponent(IKullaniciServis kullaniciServis)
+        {
+            _kullaniciServis = kullaniciServis;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            var superAdmin = _kullaniciServis.RoleGoreGetir(1);
+            var model = new ProfilViewModel
+            {
+               SuperAdmin=superAdmin
+
+            };
+
+            return View(model);
+        }
+    }
+}
