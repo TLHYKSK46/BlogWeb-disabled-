7c49e8e159e94fe90c60d961f3dac3910b91c5b1
Talha YÜKSEK <yuksek_talha@hotmail.com>
2020-09-01 13:26:53 +0300

    Hakkimda Ve küçük Tasarımsal Düzenlemeler yapıldı..

--- BlogWebUI/Controllers/HakkimdaController.cs
+++ BlogWebUI/Controllers/HakkimdaController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogBusiness.Abstract;
 using BlogWebUI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace BlogWebUI.Controllers
 {
@@ -8,8 +9,9 @@ namespace BlogWebUI.Controllers
     {
         IHakkimdaServis _hakkimdaServis;
         IKullaniciServis _kullaniciServis;
+ 
 
-        public HakkimdaController(IHakkimdaServis hakkimdaServis,IKullaniciServis kullaniciServis)
+        public HakkimdaController(IHakkimdaServis hakkimdaServis, IKullaniciServis kullaniciServis)
         {
             _hakkimdaServis = hakkimdaServis;
             _kullaniciServis = kullaniciServis;
@@ -17,16 +19,46 @@ namespace BlogWebUI.Controllers
 
         public IActionResult Index()
         {
-           
+
             var hakkimda = _hakkimdaServis.HakkimdaGetir();
             var kullanici = _kullaniciServis.KullanicilariGetir();
+      
+            List<string> deneyimlerlist = new List<string>();
+            List<string> becerilerlist = new List<string>();
+            List<string> hobilerlist = new List<string>();
+            List<string> egitimlerlist = new List<string>();
+            List<string> odullerlist = new List<string>();
+            foreach (var item in hakkimda)
+            {
+                becerilerlist=SplitList(item.Beceriler);
+                deneyimlerlist=SplitList(item.Deneyim);
+                hobilerlist = SplitList(item.Hobiler);
+                egitimlerlist = SplitList(item.Egitim);
+                odullerlist = SplitList(item.Sertifika_Oduller);
+                
+            }
             HakkimdaModel model = new HakkimdaModel
             {
-                Hakkimda = hakkimda,
-                SuperAdmin = kullanici
 
+                Hakkimda = hakkimda,
+                SuperAdmin = kullanici,
+                Deneyimler=deneyimlerlist,
+                Beceriler=becerilerlist,
+                Hobiler=hobilerlist,
+                Egitimler=egitimlerlist,
+                Oduller=odullerlist
             };
             return View(model);
         }
+        public List<string> SplitList( string kayit) {
+            List<string> splitListe = new List<string>();
+            string[] liste = kayit.Split(',');//virgüle göre ayırıcak cümleyi
+            foreach (var list in liste)
+            {
+                splitListe.Add(list.ToString());
+            }
+            return splitListe;
+        }
+        
     }
 }
--- BlogWebUI/Models/HakkimdaModel.cs
+++ BlogWebUI/Models/HakkimdaModel.cs
@@ -12,5 +12,10 @@ namespace BlogWebUI.Models
         public List<Hakkimda> Hakkimda { get; internal set; }
         public List<Kullanici> Kullanici { get; internal set; }
         public List<Kullanici> SuperAdmin { get; internal set; }
+        public List<string> Deneyimler { get; internal set; }
+        public List<string> Beceriler { get; internal set; }
+        public List<string> Oduller { get; internal set; }
+        public List<string> Egitimler { get; internal set; }
+        public List<string> Hobiler { get; internal set; }
     }
 }
