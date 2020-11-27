3034728105aa6bae9d8889327ccfae178d0d4000
Talha YÜKSEK <yuksek_talha@hotmail.com>
2020-11-28 01:48:07 +0300

    aa

---
+++ BlogWebUI/Areas/Admin/Models/SolMenuViewCompenentModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogWebUI.Areas.Admin.Models
+{
+    public class SolMenuViewCompenentModel
+    {
+        public int RolId { get; internal set; }
+    }
+}
---
+++ BlogWebUI/Areas/Admin/ViewComponents/SolMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BlogWebUI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogWebUI.Areas.Admin.ViewComponents
+{
+    public class SolMenuViewComponent:ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+          int  RolId = (int)HttpContext.Session.GetInt32("rolid");
+         
+
+            SolMenuViewCompenentModel model = new SolMenuViewCompenentModel
+            {
+             
+                RolId = RolId
+
+            };
+            return View(model);
+
+        }
+        }
+    }
